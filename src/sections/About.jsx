4886867020b8a6d65c605ed0d22436c7bedaf323
Roshan Chauhan <roshanranjput92@gmail.com>
2025-12-15 22:14:24 +0530

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -171,7 +171,7 @@ const About = () => {
           </div>
 
           <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
-            <Frameworks />
+            <Frameworks/>
           </div>
         </div>
       </div>
